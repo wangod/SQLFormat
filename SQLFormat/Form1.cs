a44634f1767e4a900d3d3a1cd3deb99734884193
ming <mingwan@futurefab.ai>
2022-06-09 09:29:54 +0800

    Resource打包

--- SQLFormat/Form1.cs
+++ SQLFormat/Form1.cs
@@ -20,6 +20,8 @@ namespace SQLFormat
         static DBInfo _dbInfo = null; 
         public Form1()
         {
+            AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(CurrentDomain_AssemblyResolve);
+
             InitializeComponent();
 
             this.Width = 880;
@@ -34,6 +36,16 @@ namespace SQLFormat
             this.tlpSqlResult.Dock = DockStyle.Fill;   
 
             InitData(AppDomain.CurrentDomain.BaseDirectory + "db.xml");
+        }
+
+        System.Reflection.Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            string dllName = args.Name.Contains(",") ? args.Name.Substring(0, args.Name.IndexOf(',')) : args.Name.Replace(".dll", "");
+            dllName = dllName.Replace(".", "_");
+            if (dllName.EndsWith("_resources")) return null;
+            System.Resources.ResourceManager rm = new System.Resources.ResourceManager(GetType().Namespace + ".Properties.Resources", System.Reflection.Assembly.GetExecutingAssembly());
+            byte[] bytes = (byte[])rm.GetObject(dllName);
+            return System.Reflection.Assembly.Load(bytes);
         }
 
         private void InitData(string filePath)
